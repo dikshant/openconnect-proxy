9b6e1437c5674167bcab33c25dd8b519f6f92fcb
Dikshant Adhikari <ad.dikshant@gmail.com>
2020-07-25 22:14:47 +0000

    remove: microsocks

--- build/entrypoint.sh
+++ build/entrypoint.sh
@@ -1,25 +1,13 @@
 #!/bin/sh
 
-/usr/local/bin/microsocks -i 0.0.0.0 -p 8889 & 
-wget https://cacerts.digicert.com/DigiCertSHA2SecureServerCA.crt.pem -O /etc/ssl/certs/digi.crt
-c_rehash
-
+tinyproxy
+sslh --http localhost:8888 --ssh localhost:22 -p 0.0.0.0:7777
 run () {
-  # Start openconnect
-  if [[ -z "${OPENCONNECT_PASSWORD}" ]]; then
-  # Ask for password
-    openconnect --juniper --no-dtls -u $OPENCONNECT_USER $OPENCONNECT_OPTIONS $OPENCONNECT_URL
-  elif [[ ! -z "${OPENCONNECT_PASSWORD}" ]] && [[ ! -z "${OPENCONNECT_MFA_CODE}" ]]; then
-  # Multi factor authentication (MFA)
-    (echo $OPENCONNECT_PASSWORD; echo $OPENCONNECT_MFA_CODE) | openconnect --no-dtls --juniper -u $OPENCONNECT_USER $OPENCONNECT_OPTIONS --passwd-on-stdin $OPENCONNECT_URL
-  elif [[ ! -z "${OPENCONNECT_PASSWORD}" ]]; then
-  # Standard authentication
-    echo $OPENCONNECT_PASSWORD | openconnect --juniper -u $OPENCONNECT_USER $OPENCONNECT_OPTIONS --no-dtls --passwd-on-stdin -v $OPENCONNECT_URL
-  fi
+  echo $OPENCONNECT_PASSWORD | openconnect --servercert pin-sha256:kzxsqW1zeKyDqJEZnsVFqgA+nXMSR2JlyejZQv+vS68= --juniper -u $OPENCONNECT_USER $OPENCONNECT_OPTIONS --no-dtls --passwd-on-stdin -v $OPENCONNECT_URL
 }
 
 until (run); do
-  echo "openconnect exited. Restarting process in 60 seconds…" >&2
-  sleep 60
+  echo "openconnect exited. Restarting process in 5 seconds…" >&2
+  sleep 5
 done
 
