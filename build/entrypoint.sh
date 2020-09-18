253184cb73c9cf6c167696b0ac9dcb8ec4c4b98c
Dikshant Adhikari <ad.dikshant@gmail.com>
2020-09-17 22:06:44 -0400

    update: pinned cert

--- build/entrypoint.sh
+++ build/entrypoint.sh
@@ -3,7 +3,7 @@
 /usr/local/bin/microsocks -i 0.0.0.0 -p 8888 & 
 
 run () {
-  echo $OPENCONNECT_PASSWORD | openconnect --servercert pin-sha256:kzxsqW1zeKyDqJEZnsVFqgA+nXMSR2JlyejZQv+vS68= --juniper -u $OPENCONNECT_USER $OPENCONNECT_OPTIONS --no-dtls --passwd-on-stdin -v $OPENCONNECT_URL
+  echo $OPENCONNECT_PASSWORD | openconnect --servercert pin-sha256:6t2nJXB+zHa/LF2YIvCO613/KdUtltxW5f7TNtblL/g= --juniper -u $OPENCONNECT_USER $OPENCONNECT_OPTIONS --no-dtls --passwd-on-stdin -v $OPENCONNECT_URL
 }
 
 until (run); do
